9ced54ef3f04e503e80f7a30d3a38e17283e1e41
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-26 22:17:32 +0900

    Define methods in Std.Iter

--- Itertools.lean
+++ Itertools.lean
@@ -23,7 +23,7 @@ and increments for each element in the original iterator.
 ```
 -/
 @[inline]
-def Std.Iterators.Iter.enumerate {α β : Type} [Iterator α Id β] [IteratorLoop α Id Id] [Finite α Id]
+def Std.Iter.enumerate {α β : Type} [Iterator α Id β] [IteratorLoop α Id Id] [Finite α Id]
     (it : Iter (α := α) β) : Iter (α := Std.Iterators.Types.Zip (Rxo.Iterator Nat) Id α β) (Nat × β) :=
   (0...it.count).iter.zip it
 
@@ -42,7 +42,7 @@ Returns 0 for an empty iterator.
 ```
 -/
 @[inline]
-def Std.Iterators.Iter.sum {α : Type} [Iterator α Id Nat] [IteratorLoop α Id Id]
+def Std.Iter.sum {α : Type} [Iterator α Id Nat] [IteratorLoop α Id Id]
     (it : Iter (α := α) Nat) : Nat :=
   it.fold (· + ·) 0
 
@@ -60,7 +60,7 @@ Returns 1 for an empty iterator (multiplicative identity).
 ```
 -/
 @[inline]
-def Std.Iterators.Iter.product {α : Type} [Iterator α Id Nat] [IteratorLoop α Id Id]
+def Std.Iter.product {α : Type} [Iterator α Id Nat] [IteratorLoop α Id Id]
     (it : Iter (α := α) Nat) : Nat :=
   it.fold (· * ·) 1
 -- #eval [2, 3, 6].iter.product
@@ -80,7 +80,7 @@ The function pre-allocates capacity based on the iterator's count for efficiency
 ```
 -/
 @[inline]
-def Std.Iterators.Iter.toHashMap {α β γ : Type} [BEq β] [Hashable β] [Iterator α Id (β × γ)] [IteratorLoop α Id Id]
+def Std.Iter.toHashMap {α β γ : Type} [BEq β] [Hashable β] [Iterator α Id (β × γ)] [IteratorLoop α Id Id]
     (it : Iter (α := α) (β × γ)) : HashMap β γ :=
   it.fold (fun acc pair ↦ acc.insert pair.fst pair.snd) (HashMap.emptyWithCapacity it.count)
 -- #eval [(2, "aaa"), (3, "three"), (6, "six")].iter.toHashMap
@@ -99,7 +99,7 @@ The function pre-allocates capacity based on the iterator's count for efficiency
 ```
 -/
 @[inline]
-def Std.Iterators.Iter.toHashSet {α β : Type} [BEq β] [Hashable β] [Iterator α Id β] [IteratorLoop α Id Id]
+def Std.Iter.toHashSet {α β : Type} [BEq β] [Hashable β] [Iterator α Id β] [IteratorLoop α Id Id]
     (it : Iter (α := α) β) : HashSet β :=
   it.fold (·.insert ·) (HashSet.emptyWithCapacity it.count)
 -- #eval [2, 3, 6].iter.toHashSet
